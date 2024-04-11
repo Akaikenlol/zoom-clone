c72b3c3a90e7e4aa8dda29b8cf4b7a30f5e3a07e
Akaikenlol <keithken972@gmail.com>
2024-04-11 21:11:46 +0630

    feat: Navbar chore: required images and icons

--- app/(root)/(home)/layout.tsx
+++ app/(root)/(home)/layout.tsx
@@ -1,11 +1,13 @@
+import Navbar from "@/components/Navbar";
+import Sidebar from "@/components/Sidebar";
 import React from "react";
 
 const HomeLayout = ({ children }: { children: React.ReactNode }) => {
 	return (
 		<main className="relative">
-			Navbar
+			<Navbar />
 			<div className="flex">
-				Sidebar
+				<Sidebar />
 				<section className="flex min-h-screen flex-1 flex-col px-6 pb-6 pt-6 max-md:pb-14 sm:px-14">
 					<div className="w-full">{children}</div>
 				</section>
