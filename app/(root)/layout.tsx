e2ab8264848ac85c1ac494b58691f469a34c8eca
Akaikenlol <keithken972@gmail.com>
2024-04-14 21:51:04 +0630

    feat: Add Stream Video Provider and Toaster component chore: npm install toaster , stream

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,7 +1,12 @@
+import StreamVideoProvider from "@/providers/StreamClientProvider";
 import React from "react";
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
-	return <main>{children}</main>;
+	return (
+		<main>
+			<StreamVideoProvider>{children}</StreamVideoProvider>
+		</main>
+	);
 };
 
 export default RootLayout;
