c5299b24a71978f985178408d80eb2ad107778c0
Akaikenlol <keithken972@gmail.com>
2024-04-16 21:02:04 +0630

    feat: Meeting Room Implementation chore: npm i dropdown-menu

--- components/MeetingRoom.tsx
+++ components/MeetingRoom.tsx
@@ -1,12 +1,27 @@
 "use client";
 
-import { PaginatedGridLayout, SpeakerLayout } from "@stream-io/video-react-sdk";
+import { cn } from "@/lib/utils";
+import {
+	CallControls,
+	CallParticipantsList,
+	PaginatedGridLayout,
+	SpeakerLayout,
+} from "@stream-io/video-react-sdk";
 import React, { useState } from "react";
+import {
+	DropdownMenu,
+	DropdownMenuContent,
+	DropdownMenuItem,
+	DropdownMenuLabel,
+	DropdownMenuSeparator,
+	DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 type CallLayoutType = "grid" | "speaker-left" | "speaker-right";
 
 const MeetingRoom = () => {
 	const [layout, setLayout] = useState<CallLayoutType>("speaker-left");
+	const [showParticipants, setShowParticipants] = useState(false);
 
 	const CallLayout = () => {
 		switch (layout) {
@@ -25,6 +40,29 @@ const MeetingRoom = () => {
 				<div className="flex size-full max-w-[1000px] items-center">
 					<CallLayout />
 				</div>
+				<div
+					className={cn("h-[calc(100vh-86px)] hidden ml-2", {
+						"show-block": showParticipants,
+					})}
+				>
+					<CallParticipantsList onClose={() => setShowParticipants(false)} />
+				</div>
+			</div>
+			<div className="fixed bottom-0 flex w-full items-center justify-center gap-5">
+				<CallControls />
+				<DropdownMenu>
+					<div className="flex items-center">
+						<DropdownMenuTrigger>Open</DropdownMenuTrigger>
+					</div>
+					<DropdownMenuContent>
+						<DropdownMenuLabel>My Account</DropdownMenuLabel>
+						<DropdownMenuSeparator />
+						<DropdownMenuItem>Profile</DropdownMenuItem>
+						<DropdownMenuItem>Billing</DropdownMenuItem>
+						<DropdownMenuItem>Team</DropdownMenuItem>
+						<DropdownMenuItem>Subscription</DropdownMenuItem>
+					</DropdownMenuContent>
+				</DropdownMenu>
 			</div>
 		</section>
 	);
